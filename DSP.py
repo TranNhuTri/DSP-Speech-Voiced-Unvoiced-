c9d744c9ed3af86e7ebb49e2130b5ed73dfe0732
Tran Nhu Tri <trannhutri0703@gmail.com>
2021-10-09 21:45:56 +0700

    add speech silent discrimination

--- DSP.py
+++ DSP.py
@@ -41,10 +41,18 @@ class Wave:
 
 		return [t, ste]
 
-	def detectSpeechSilent(self, T, minLenSilent = 0.3): # hàm tách khoảng lặng âm thanh
-		f = [] # mảng chứa index STE của các vùng khoảng lặng
-		g = [] # mảng chứa index STE của các vùng tiếng nói
-		Si = [] # mảng chứa index STE vùng khoảng lặng tạm thời
+	def PlotSTE(self):
+		t, ste = self.STE()
+		T = 1e9
+		# plt.plot(self.times, self.amplitudes/max(self.amplitudes), '#2b80ffe0')
+		# plt.plot([0, t[-1]], [T, T], '#ff792be8')
+		plt.plot(t, ste, '#ff0000')
+		plt.show()
+
+	def DetectSpeechSilent(self, T, minLenSilent = 0.3): # hàm tách khoảng lặng âm thanh
+		silent = [] # mảng chứa index STE của các vùng khoảng lặng
+		speech = [] # mảng chứa index STE của các vùng tiếng nói
+		tmpSi = [] # mảng chứa index STE vùng khoảng lặng tạm thời
 		t, ste = self.STE()
 
 		timeStep = 1/self.Fs # chu kì :))
@@ -52,42 +60,117 @@ class Wave:
 		isFirstSilentRegion = True
 		for i in range(len(ste)): # tách khoảng lặng
 			if ste[i] < T:
-				Si.append(i)
+				tmpSi.append(i)
 			else:
-				if len(Si) != 0 and t[Si[-1]] - t[Si[0]] + timeStep >= minLenSilent or isFirstSilentRegion == True:
-					f.append(Si)
+				if len(tmpSi) != 0 and t[tmpSi[-1]] - t[tmpSi[0]] + timeStep >= minLenSilent or isFirstSilentRegion == True:
+					silent.append(tmpSi)
 				isFirstSilentRegion = False
-				Si = []
+				tmpSi = []
 
-		if len(Si) != 0 and t[Si[-1]] - t[Si[0]] + timeStep >= minLenSilent:
-			f.append(Si)
+		if len(silent) != 0 and t[tmpSi[-1]] - t[tmpSi[0]] + timeStep >= minLenSilent:
+			silent.append(tmpSi)
 
-		if len(f) > 0: # tách khoảng tiếng nói
+		if len(silent) > 0: # tách khoảng tiếng nói
 			pre = 0
-			for i in range(0, len(f)):
-				for j in range(pre, f[i][0]):
-					g.append(j)
-				pre = f[i][-1]
-		for i in range(f[-1][-1], len(ste)):
-			g.append(i)
+			for i in range(0, len(silent)):
+				for j in range(pre, silent[i][0]):
+					speech.append(j)
+				pre = silent[i][-1]
+		for i in range(silent[-1][-1], len(ste)):
+			speech.append(i)
 
-		return [f, g]
+		return [silent, speech]
 
-	def plotSTE(self):
+	def DetectOverlapSTE(self, silent, speech):
 		t, ste = self.STE()
-		T = 1e9
-		# plt.plot(self.times, self.amplitudes/max(self.amplitudes), '#2b80ffe0')
-		plt.plot([0, t[-1]], [T, T], '#ff792be8')
-		plt.plot(t, ste, '#ff0000')
+
+		Tmax = 0
+		Tmin = max(ste)
+
+		for i in silent:
+			for j in i:
+				Tmax = max(Tmax, ste[j])
+		for i in speech:
+			Tmin = min(Tmin, ste[i])
+
+		f = [] # mảng chứa STE của vùng lặng trong overlap
+		for i in silent:
+			for j in i:
+				if ste[j] > Tmin and ste[j] < Tmax:
+					f.append(ste[j])
+		g = [] # mảng chứa STE của vùng tiếng nói trong overlap
+		for i in speech:
+			if ste[i] > Tmin and ste[i] < Tmax:
+				g.append(ste[j])
+
+		return [f, g, Tmin, Tmax]
+
+	def STEThreshold(self, T = 1e9):
+
+		silent, speech = self.DetectSpeechSilent(T)
+
+		f, g, Tmin, Tmax = self.DetectOverlapSTE(silent, speech)
+
+		if len(f) == 0 or len(g) == 0:
+			print("Break")
+			return T
+
+		Tmid = (Tmax + Tmin)/2
+
+		i = len([i for i in f if i < Tmid])
+		p = len([i for i in g if i > Tmid])
+		j = -1
+		q = -1
+		while i != j or p != q:
+			value = sum([max(i - Tmid, 0) for i in f])/len(f) - sum([max(Tmid - i, 0) for i in g])/len(g)
+			if value > 0:
+				Tmin = Tmid
+			else:
+				Tmax = Tmid
+			Tmid = (Tmax + Tmin)/2
+			j = i
+			q = p
+			i = len([i for i in f if i < Tmid])
+			p = len([i for i in g if i > Tmid])
+
+		return Tmid
+
+	def SpeechSilentDiscrimination(self):
+		n = self.times
+		T = self.STEThreshold();
+		f, g = self.DetectSpeechSilent(T)
+
+		t, ste = self.STE()
+
+		fig, [plt1, plt2] = plt.subplots(2, 1)
+
+		for i in f:
+			start, end = i[0], i[-1]
+
+			plt1.plot([t[start], t[start]], [0, max(ste)], '#00ca06e0')
+			plt1.plot([t[end], t[end]], [0, max(ste)], '#d87bff')
+
+			plt2.plot([t[start], t[start]], [-1, 1], '#00ca06e0')
+			plt2.plot([t[end], t[end]], [-1, 1], '#d87bff')
+
+			print(t[start], t[end], end = " ")
+
+		print()
+
+		plt1.plot([0, n[-1]], [T, T], '#ff792be8')
+		plt1.plot(t, ste, '#2b80ffe0')
+
+		data = self.amplitudes
+
+		plt2.plot(n, data/max(data), '#2b80ffe0')
+		plt2.plot(t, ste/max(ste), '#ff0000')
+
 		plt.show()
 
 
 def main():
 	wave = Wave()
-	wave.plotSTE()
-	f, g = wave.detectSpeechSilent(T = 1e9)
-	print(len(f))
-	print(len(g))
+	wave.SpeechSilentDiscrimination()
 
 if __name__ == '__main__':
 	main()
